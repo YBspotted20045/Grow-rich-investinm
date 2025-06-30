5a5d7a200563e69138ca088affa826b53552cc86
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-06-30 20:45:44 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Grow Rich Investments</h1>
+      <p>Welcome to the platform. Let’s grow your money together!</p>
+    </div>
+  )
+}
+
+export default App
