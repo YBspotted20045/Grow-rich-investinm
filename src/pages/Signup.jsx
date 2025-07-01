102af6199635a7d004d03be8655164171fa1d4c2
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-07-01 21:14:15 +0100

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-// src/pages/Signup.jsx
+import './Signup.css';// src/pages/Signup.jsx
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import API from "../axios";
