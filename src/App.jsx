884ec073598cf78580de73053f63b8abe2f2150b
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-08-07 17:10:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-// src/App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Withdrawal from './pages/Withdrawal';
 
-const App = () => {
-  const isLoggedIn = !!localStorage.getItem("token");
-
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/signup" />} />
+        <Route path="/" element={<Signup />} /> {/* Default route */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />} />
-        <Route path="*" element={<h2>404 | Page Not Found</h2>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/withdraw" element={<Withdrawal />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
