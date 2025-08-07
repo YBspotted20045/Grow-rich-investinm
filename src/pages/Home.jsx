d44d92e43f4435f60411463b4e001ec24de0d669
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-08-07 17:43:57 +0100

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Welcome to GrowRich Investments</h1>
+      <p>You're logged in successfully.</p>
+    </div>
+  );
+};
+
+export default Home;
