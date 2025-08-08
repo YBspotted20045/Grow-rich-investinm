337fd592081d133b20cfdcf9375038c97626815c
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-08-08 12:07:19 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
+// src/axios.js
+
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://grow-0nfm.onrender.com/api', // Your live backend URL
+  baseURL: 'https://grow-0nfm.onrender.com', // ✅ your backend URL
 });
 
 export default API;
