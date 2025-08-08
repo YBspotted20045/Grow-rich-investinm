d291dbfc1cc45ddbee560ebff2e02be6588274ab
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-08-08 14:34:07 +0100

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-import "./signup.css";
+import "./Signup.css";
 import axios from "axios";
 
 const Signup = () => {
