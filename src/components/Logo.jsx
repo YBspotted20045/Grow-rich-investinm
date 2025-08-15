30a2a0466f4764b1d049412ecd9177bc88f98b63
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-08-15 14:42:58 +0100

    Create Logo.jsx

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <img src="/logo.png" alt="GrowRichInvestments Logo" className="animated-logo" />
+    </div>
+  );
+};
+
+export default Logo;
