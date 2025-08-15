8c2a6e24aca485591335cc538d505724bb231d20
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-08-15 06:40:18 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 // src/pages/Home.jsx
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+import API from "../style/axios";
 
 const Home = () => {
   const [user, setUser] = useState(null);
