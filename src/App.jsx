357fe122acc529ded1f5f266e45230e886c2a7f8
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-08-15 13:51:02 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AnimatedLogo from "./components/AnimatedLogo";
+import Landing from "./pages/Landing";
+
+function App() {
+  return (
+    <div>
+      <AnimatedLogo />
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
