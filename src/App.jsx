692df8e21748b8267c574fcf61e953af0618217e
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-08-19 10:42:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
+
+// Importing pages (not components)
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Default: open Signup */}
+        {/* Default route */}
         <Route path="/" element={<Navigate to="/signup" />} />
+        
+        {/* Pages */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
