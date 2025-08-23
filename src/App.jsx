5e9c56b94838937f12b40c96b442cbb2ec8c538d
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-08-23 23:12:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-// Importing pages (not components)
+// Importing pages
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard"; // <-- make sure this path is correct
 
 function App() {
   return (
@@ -13,9 +14,12 @@ function App() {
         {/* Default route */}
         <Route path="/" element={<Navigate to="/signup" />} />
         
-        {/* Pages */}
+        {/* Auth Pages */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        
+        {/* Dashboard Page */}
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
