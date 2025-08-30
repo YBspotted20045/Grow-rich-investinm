8b0baa4f5257b8935351e4d41d8e5245df444874
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-08-30 19:07:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import BottomNav from "./components/BottomNav";
 
 // PAGES (I’ll send the rest next)
-import Landing from "./pages/Landing.jsx";
+import Landing from "./Pages/Landing.jsx";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import Dashboard from "./Pages/Dashboard";
