daf6b0e4f3bb408a0144a8e27307a5a667111270
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-09-01 11:45:15 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,41 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
 import Dashboard from "./Pages/Dashboard";
 import ReferralDashboard from "./Pages/ReferralDashboard";
-import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
+  const token = localStorage.getItem("gr_token");
+
   return (
-    <Router>
-      <Routes>
-        {/* Default Route */}
-        <Route
-          path="/"
-          element={
-            localStorage.getItem("gr_token") ? (
-              <Navigate to="/dashboard" replace />
-            ) : (
-              <Navigate to="/login" replace />
-            )
-          }
-        />
+    <Routes>
+      {/* Default Route */}
+      <Route
+        path="/"
+        element={
+          token ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />
+        }
+      />
 
-        {/* Public Routes */}
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+      {/* Public Routes */}
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
 
-        {/* Protected Routes */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/referrals"
-          element={
-            <ProtectedRoute>
-              <ReferralDashboard />
-            </ProtectedRoute>
-          }
-        />
+      {/* Protected Routes */}
+      <Route
+        path="/dashboard"
+        element={token ? <Dashboard /> : <Navigate to="/login" replace />}
+      />
+      <Route
+        path="/referrals"
+        element={token ? <ReferralDashboard /> : <Navigate to="/login" replace />}
+      />
 
-        {/* Catch-all route */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
+      {/* Catch-all route */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
