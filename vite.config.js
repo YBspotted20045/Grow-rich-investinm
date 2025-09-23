58ad684e4722656ad6d0f795e08c0ea42076bf55
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-09-23 22:30:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,10 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "src"), // safer alias
     },
+  },
+  build: {
+    outDir: "dist", // explicitly tell Vite to build to 'dist'
   },
 });
