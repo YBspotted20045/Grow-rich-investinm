2d812aa6e53769f69dbb48ec55efeac8162b46ad
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-09-24 11:08:44 +0100

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "../axios.js";
+import axios from "./axios.js";
 import "./AdminDashboard.css";
 
 const AdminDashboard = () => {
