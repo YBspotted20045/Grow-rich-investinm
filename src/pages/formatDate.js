ab09bfe4215f682ec386408e9410a35730287949
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-09-24 10:44:53 +0100

    Create formatDate.js

---
+++ src/pages/formatDate.js
@@ -0,0 +1,5 @@
+// src/pages/formatDate.js
+export const formatDate = (dateString) => {
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+};
