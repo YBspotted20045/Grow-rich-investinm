a0c455d45debd5a378f00f27e33e6e625430ec43
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-09-24 21:16:36 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,22 @@
-// src/index.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// src/index.jsx  
+import React from "react";  
+import ReactDOM from "react-dom/client";  
+import { BrowserRouter, Routes, Route } from "react-router-dom";  
 
-import Home from "./pages/Home.jsx";
-import Signup from "./pages/Signup.jsx";
-import Login from "./pages/Login.jsx";
+import Home from "./pages/Home.jsx";  
+import Signup from "./pages/Signup.jsx";  
+import Login from "./pages/Login.jsx";  
+import Dashboard from "./pages/Dashboard.jsx"; // 👈 make sure this file exists  
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));  
 
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
-  </BrowserRouter>
+root.render(  
+  <BrowserRouter>  
+    <Routes>  
+      <Route path="/" element={<Home />} />  
+      <Route path="/login" element={<Login />} />  
+      <Route path="/signup" element={<Signup />} />  
+      <Route path="/Dashboard" element={<Dashboard />} /> {/* 👈 added route */}  
+    </Routes>  
+  </BrowserRouter>  
 );
