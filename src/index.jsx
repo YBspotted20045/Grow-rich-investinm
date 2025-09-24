db8e8e31c22614eae7bd6b41191a8d11eabd6287
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-09-24 19:58:39 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import Home from "./Home";  // adjust if default export
-import Signup from "./Signup";
-import Login from "./Login";
-import Dashboard from "./Dashboard";
+import Home from "./pages/Home.jsx";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
