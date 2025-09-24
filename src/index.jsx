af7448d57ab32e881d78c8737b627de7f2f60cd2
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-09-24 20:35:17 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,20 @@
+// src/index.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home.jsx";
 import Signup from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   </BrowserRouter>
 );
