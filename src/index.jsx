f66a8714adbd9ce9f35aec2a6573877d9ff00743
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-09-24 20:06:59 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Signup from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
