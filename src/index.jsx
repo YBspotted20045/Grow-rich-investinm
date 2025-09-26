da4bdc9220bc1a166574a4dd112275a76e23faef
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-09-26 02:08:52 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,17 +6,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";  
 import Signup from "./pages/Signup.jsx";  
 import Login from "./pages/Login.jsx";  
-import Dashboard from "./pages/Dashboard.jsx"; // 👈 make sure this file exists  
+import Dashboard from "./pages/Dashboard.jsx";  
+import ProtectedRoute from "./components/ProtectedRoute.jsx"; // 👈 added this  
 
 const root = ReactDOM.createRoot(document.getElementById("root"));  
 
 root.render(  
   <BrowserRouter>  
     <Routes>  
+      {/* Public routes */}  
       <Route path="/" element={<Home />} />  
       <Route path="/login" element={<Login />} />  
       <Route path="/signup" element={<Signup />} />  
-      <Route path="/Dashboard" element={<Dashboard />} /> {/* 👈 added route */}  
+
+      {/* Protected route for dashboard */}  
+      <Route  
+        path="/dashboard"  
+        element={  
+          <ProtectedRoute>  
+            <Dashboard />  
+          </ProtectedRoute>  
+        }  
+      />  
     </Routes>  
   </BrowserRouter>  
 );
