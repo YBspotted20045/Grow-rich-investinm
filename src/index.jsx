051b041423ad24b6e630292c622e2de7141c0e1e
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-09-27 02:48:52 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,53 +1,60 @@
-// src/index.jsx  
-import React from "react";  
-import ReactDOM from "react-dom/client";  
-import { BrowserRouter, Routes, Route } from "react-router-dom";  
+// src/index.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home.jsx";  
-import Signup from "./pages/Signup.jsx";  
-import Login from "./pages/Login.jsx";  
-import Dashboard from "./pages/Dashboard.jsx";  
-import Deposit from "./pages/Deposit.jsx";  
-import Withdraw from "./pages/Withdrawal.jsx";   // 👈 add this
-import ProtectedRoute from "./components/ProtectedRoute.jsx";  
+import Home from "./pages/Home.jsx";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Deposit from "./pages/Deposit.jsx";
+import Withdrawal from "./pages/Withdrawal.jsx"; // 👈 fixed naming
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Layout from "./components/Layout.jsx"; // 👈 add this
 
-const root = ReactDOM.createRoot(document.getElementById("root"));  
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(  
-  <BrowserRouter>  
-    <Routes>  
-      {/* Public routes */}  
-      <Route path="/" element={<Home />} />  
-      <Route path="/login" element={<Login />} />  
-      <Route path="/signup" element={<Signup />} />  
+root.render(
+  <BrowserRouter>
+    <Routes>
+      {/* Public routes */}
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
 
-      {/* Protected routes */}  
-      <Route  
-        path="/dashboard"  
-        element={  
-          <ProtectedRoute>  
-            <Dashboard />  
-          </ProtectedRoute>  
-        }  
-      />  
+      {/* Protected routes (all inside Layout) */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Layout>
+              <Dashboard />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
 
-      <Route  
-        path="/deposit"  
-        element={  
-          <ProtectedRoute>  
-            <Deposit />  
-          </ProtectedRoute>  
-        }  
-      />  
+      <Route
+        path="/deposit"
+        element={
+          <ProtectedRoute>
+            <Layout>
+              <Deposit />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
 
-      <Route  
-        path="/withdraw"  
-        element={  
-          <ProtectedRoute>  
-            <Withdraw />  
-          </ProtectedRoute>  
-        }  
-      />  
-    </Routes>  
-  </BrowserRouter>  
+      <Route
+        path="/withdraw"
+        element={
+          <ProtectedRoute>
+            <Layout>
+              <Withdrawal />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
 );
